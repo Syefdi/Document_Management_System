3861ad679f5ce12c12e66e4617b66dd5134f47b3
Hairudin DJ. <97005385+gtek2022@users.noreply.github.com>
2025-08-08 11:30:24 +0700

    int

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => (string) Str::uuid(), // generate UUID
+            'firstName' => 'admin',
+            'lastName' => '',
+            'isDeleted' => 0,
+            'userName' => 'admin',
+            'normalizedUserName' => 'ADMIN',
+            'email' => 'admin@admin.com',
+            'normalizedEmail' => 'ADMIN@ADMIN.COM',
+            'emailConfirmed' => 1,
+            'password' => Hash::make('123456'),
+            'securityStamp' => (string) Str::uuid(),
+            'concurrencyStamp' => (string) Str::uuid(),
+            'phoneNumber' => null,
+            'phoneNumberConfirmed' => 0,
+            'twoFactorEnabled' => 0,
+            'lockoutEnd' => null,
+            'lockoutEnabled' => 0,
+            'accessFailedCount' => 0,
+            'resetPasswordCode' => null,
+        ]);
+
+        $this->call([
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            LanguageSeeder::class,
+            PermissionSeederV2::class,
+            PermissionSeederV21::class,
+            PermissionSeederV22::class,
+            PermissionSeederV23::class,
+            PermissionSeederV24::class,
+            PermissionSeederV30::class,
+            PermissionSeederV31::class,
+            PermissionSeederV40::class,
+            PermissionSeederV50::class
+        ]);
+    }
+}
