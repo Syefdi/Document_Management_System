bc647a41adfa0928c5219a9bbba89268bda9c75f
Irfan-Arimiwandi <irfanarimiwandi@gmail.com>
2025-08-18 23:12:15 +0700

    reminder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\NotificationScheduleInterface;
+use App\Repositories\Implementation\NotificationScheduleRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-       
+        $this->app->bind(
+            NotificationScheduleInterface::class,
+            NotificationScheduleRepository::class
+        );
     }
 
     /**
