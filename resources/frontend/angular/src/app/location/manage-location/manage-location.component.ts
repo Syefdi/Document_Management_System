b9b9c54f9f45b84ef02fffb17ca3eaa8b073d35a
syefdi <ssyefdi@gmail.com>
2025-09-17 11:59:24 +0700

    fix location edit

--- resources/frontend/angular/src/app/location/manage-location/manage-location.component.ts
+++ resources/frontend/angular/src/app/location/manage-location/manage-location.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -14,6 +14,10 @@ import { SharedModule } from '@shared/shared.module';
 import { FeatherModule } from 'angular-feather';
 import { BaseComponent } from 'src/app/base.component';
 import { LocationStore } from '../location-store';
+import { LocationService } from '../location.service';
+import { CommonError } from '@core/error-handler/common-error';
+import { ToastrService } from 'ngx-toastr';
+import { Subject, takeUntil } from 'rxjs';
 import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -36,41 +40,89 @@ import { toObservable } from '@angular/core/rxjs-interop';
   templateUrl: './manage-location.component.html',
   styleUrl: './manage-location.component.scss'
 })
-export class ManageLocationComponent extends BaseComponent implements OnInit {
+export class ManageLocationComponent extends BaseComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  
   location: Location;
   locationForm: UntypedFormGroup;
   isEditMode = false;
+  locationId: string | null = null;
+  loading = false;
 
   private fb = inject(UntypedFormBuilder);
   private router = inject(Router);
   private activeRoute = inject(ActivatedRoute);
+  private locationService = inject(LocationService);
+  private toastr = inject(ToastrService);
   public locationStore = inject(LocationStore);
 
   constructor() {
     super();
-    this.subscribeIsAddUpdate();
+    this.initializeForm();
   }
 
   ngOnInit(): void {
-    this.createLocationForm();
+    this.checkRouteParams();
     this.subscribeIsAddUpdate();
-    this.sub$.sink = this.activeRoute.data.subscribe(
-      (data: { location: Location }) => {
-        if (data.location) {
-          this.isEditMode = true;
-          this.locationForm.patchValue(data.location);
-          this.location = data.location;
-        }
-      });
   }
 
-  createLocationForm() {
+  override ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private initializeForm(): void {
     this.locationForm = this.fb.group({
       id: [''],
       name: ['', [Validators.required]],
       description: [''],
       address: [''],
     });
+  }
+
+  private checkRouteParams(): void {
+    this.locationId = this.activeRoute.snapshot.paramMap.get('id');
+    if (this.locationId) {
+      this.isEditMode = true;
+      this.loadLocation();
+    }
+  }
+
+  private loadLocation(): void {
+    if (!this.locationId) return;
+    
+    this.loading = true;
+    this.locationService.getLocation(this.locationId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (result) => {
+          if (result && typeof result === 'object' && 'messages' in result) {
+            const error = result as CommonError;
+            if (error.error && error.error['message']) {
+               this.toastr.error(error.error['message']);
+            } else if (error.messages && error.messages.length > 0) {
+              this.toastr.error(error.messages[0]);
+            } else {
+              this.toastr.error('An error occurred');
+            }
+          } else {
+            const location = result as Location;
+            this.locationForm.patchValue({
+              id: location.id,
+              name: location.name,
+              description: location.description,
+              address: location.address
+            });
+            this.location = location;
+          }
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error('Error loading location:', error);
+          this.toastr.error('Failed to load location data');
+          this.loading = false;
+        }
+      });
   }
 
   private markFormGroupTouched(formGroup: UntypedFormGroup) {
